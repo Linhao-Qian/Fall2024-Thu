3485f8ae3fee90a78342e595e9c9afef1cb959f5
Linhao-Qian <17768709606@163.com>
2024-10-24 12:45:28 -0700

    dynamic sizing in Header component, updated app.json to allow landscape as well

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, useWindowDimensions, View } from 'react-native'
 import React from 'react'
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 export default function Header({name}) {
+  const { width, height } = useWindowDimensions();
+
   return (
     <View>
-      <Text style={styles.text}>Welcome to {name}</Text>
+      <Text style={[styles.text, { paddingVertical: height < 415 ? 0 : 10 }]}>
+        Welcome to {name}
+      </Text>
     </View>
   )
 }
@@ -12,7 +19,7 @@ export default function Header({name}) {
 const styles = StyleSheet.create({
   text: {
     color: "purple",
-    fontSize: 25,
+    fontSize: windowWidth < 380 ? 20 : 26,
     borderColor: "purple",
     borderWidth: 2,
     padding: 5,
